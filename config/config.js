5ed3a3debbcc9e76bb01c139a5bffc28063e93da
ibrahim aljeddawi <algedawy89@gmail.com>
2024-09-18 03:21:38 +0300

    sechond commit

---
+++ config/config.js
@@ -0,0 +1,92 @@
+﻿Config({
+    "network-name": "السعيدة",
+    "service-number": "772024038",
+    "login-type": "passwordAsUser",
+    "news-line": " <span style='color:rgb(61, 43, 31)'>.::</span>  تنوية : لقد تم اضافة استراحة للشبكة (  <span style='background:white;color:red;border-radius:12px;'>بث مباشر</span> - مسلسلات - افلام - برامج تلفزيونية - اسلاميات)   <span style='color:rgb(61, 43, 31);margin:0px 2px' >e.net</span>  <span style='color:rgb(61, 43, 31)'>::.</span> " ,
+    "input-autocomplete": "on",
+    "input-type": "text",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-text-type-when": 0,
+    "enable-hot-cookie": 1 ,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح   {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field":true ,
+    "loan-button": false,
+    "redirect-to-esterahah": "",// orWithProtocol: ex, https://google.com leave it blank if dont need esterahah button
+    "redirect-to-mobasher": "",// orWithProtocol: ex, https://google.com leave it blank if dont need mobasher button
+    "app-store-base-url": "",// orWithProtocol: ex, http://192.168.10.10/app/ // leave it blank if locally
+    "profiles": [
+        {
+            "price": " (استراحة فقط) 100 ريال",
+            "time": "12 ساعه",
+            "transfer": "-----",
+            "validity": "يوم",
+        },
+        {
+            "price": "100 ريال",
+            "time": "12 ساعة",
+            "transfer": "300 ميجا",
+            "validity": "يوم",
+        },
+        {
+            "price": "200 ريال",
+            "time": "24 ساعة",
+            "transfer": "700 ميجا",
+            "validity": "يومين",
+        },
+        {
+            "price": "300 ريال",
+            "time": "36 ساعة",
+            "transfer": "1 جيجا",
+            "validity": "3 ايام",
+        },
+        {
+            "price": "500 ريال",
+            "time": "60 ساعه",
+            "transfer": "1.8 جيجا",
+            "validity": "5 ايام",
+        },
+		 {
+            "price": "1500 ريال",
+            "time": "مفتوح",
+            "transfer": "6 جيجا",
+            "validity": "30 يوم",
+        },
+
+		 {
+            "price": "3000 ريال",
+            "time": "مفتوح",
+            "transfer": "14 جيجا",
+            "validity": "30 يوم",
+        }
+
+	
+       
+    ],
+    "sell-points": [
+        {"name":"......  جميع البقالات المجاورة للشبكة  ......"},
+        {"name":"بقالة القواس"},
+        {"name":"بقالة الخطيب"},
+        {"name":"النعطي للمعسل "},
+        {"name":"بقالة مالك"},
+        {"name":"بقالة اشرف الملحم"},
+        {"name":"بقالة سعيد"},
+        {"name":"بقالة محمد سعيد"},
+        {"name":"بيت المزاج للمعسل"}
+        
+    ]
+})
---
+++ config/sell-points.js
@@ -0,0 +1,4 @@
+setConfig(document.currentScript,'{"name":"sell point 1"}')
+setConfig(document.currentScript,'{"name":"sell point 2"}')
+setConfig(document.currentScript,'{"name":"sell point 3"}')
+setConfig(document.currentScript,'sell-points')
